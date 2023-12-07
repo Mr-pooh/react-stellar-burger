ab0537eb984cb702856a7890cb1bd64b00d0ba1b
Nikita Bokov <nekit123333@yandex.ru>
2023-12-07 12:38:50 +0300

    Небольшие правки

--- src/components/orders/orderElement/orderElement.jsx
+++ src/components/orders/orderElement/orderElement.jsx
@@ -8,24 +8,24 @@ export default function OrderElement({item}){
 
     const location = useLocation()
 
-
+ 
 
     return(
     <Link
-      to={`/feed/${item._id}`}
+      to={`/feed/${item.number}`}
       state={{ background: location }}
       className={styles.link}
     >
       <li className={styles.elem} >
         <div className={styles.header}>
           <h3 className={`text text_type_digits-default`}>#{item.number}</h3>
-          <p className={`text text_type_main-default text_color_inactive`}><FormattedDate date={new Date(item.createdAt)}  />  i-GMT+3</p>
+          <p className={`text text_type_main-default text_color_inactive`}><FormattedDate date={new Date(item.createdAt)}  /> i-GMT+3</p>
         </div>
-        <h1 className={`text text_type_main-smal`}>{item.name}</h1>
-        <div>
-          <div></div>
+        <h1 className={styles.name +` text text_type_main-smal`}>{item.name}</h1>
+        <div className={styles.ingredientContainer}>
+          <div className={styles.imageIngr}>sssssssssssssssssss</div>
           <div className={styles.price}>
-          <h4 className="text text_type_digits-default p-1">{item.price}</h4>
+            <h4 className="text text_type_digits-default p-1">250</h4>
             <CurrencyIcon type="primary" />
           </div>
         </div>
